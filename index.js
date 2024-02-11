ea1afc378730eb715901a10cc81f68487a07fe25
Abhishek Gupta <abhigpta19@gmail.com>
2024-02-11 17:15:58 +0530

    redis done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const Redis = require('ioredis');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const cors = require('cors');
@@ -9,6 +10,16 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const port = 8000;
+const redis = new Redis({
+  host: 'localhost',
+  port: 6379,
+});
+
+// Connect to the second Redis instance on port 6380
+const redisimg = new Redis({
+  host: 'localhost',
+  port: 6379,
+});
 
 app.use(express.json({limit: '2000mb'}));
 app.use(express.urlencoded({extended: true, limit:'2000mb'}));
@@ -18,39 +29,16 @@ app.use(cors())
 const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage, limits: { fileSize: 1024 * 1024 * 4 * 1024} });
 
-function savepost(post)
-{
-  const filePath = 'data.json';
 
-  // New JSON object to append
-  const newData = post;
-
-  // Read the existing JSON file
-  fs.readFile(filePath, 'utf8', (readErr, data) => {
-    if (readErr) {
-      console.error('Error reading the file:', readErr);
-      return;
+function savepost(id,post)
+{    try 
+    {
+      redis.set(id,JSON.stringify(post));
+    } 
+    catch (err) 
+    {
+      console.error('Database error', err);
     }
-
-    try {
-      // Parse the existing JSON data
-      const existingData = JSON.parse(data);
-
-      // Append the new data to the array
-      existingData.push(newData);
-
-      // Write the updated data back to the file
-      fs.writeFile(filePath, JSON.stringify(existingData, null, 2), 'utf8', (writeErr) => {
-        if (writeErr) {
-          console.error('Error writing to the file:', writeErr);
-        } else {
-          console.log('Data appended and file updated successfully.');
-        }
-      });
-    } catch (parseErr) {
-      console.error('Error parsing JSON:', parseErr);
-    }
-  });
 }
 
 app.post('/clickCount', async(req, res) => {
@@ -152,19 +140,21 @@ app.post('/likesCount/:id', async(req, res) => {
 app.post('/createPost', async(req, res) => {
   console.log("hello ji my name is jassi");
   console.log(req.body);
-  req.body.id = uuidv4();
-  savepost(req.body);
+  const ids = uuidv4();
+  savepost(ids,req.body);
   res.json({msg : "post saved successfully"});
 });
 
-app.post('/upload', upload.single('image'), (req, res) => {
-  console.log("majhfjdhfjd");
-  const imagePath = `./images/${Date.now()}_${req.file.originalname}`;
-  fs.writeFileSync(imagePath, req.file.buffer);
-  const name = path.basename(imagePath);
+app.post('/upload', upload.single('image'), async(req, res) => {
+  const imageBuffer = req.file.buffer;
+  let imgid = uuidv4();
+  imgid = "img" + imgid;
+  await redisimg.set(imgid,imageBuffer);
+  console.log("image stored successfully");
+  console.log(typeof req.file.buffer)
   res.json({ 
     message: 'Image uploaded successfully',
-    name: name
+    name: imgid
   });
 });
 
@@ -181,71 +171,109 @@ app.post('/uploadFile', upload.single('desc'), (req, res) => {
 });
 
 app.get('/getAllDataById/:id', async (req, res) => {
-  const filePath = 'data.json';
-
-  // Read the existing JSON file
-  fs.readFile(filePath, 'utf8', (readErr, data) => {
-    if (readErr) {
-      console.error('Error reading the file:', readErr);
-      return;
-    }
-
-    const existingData = JSON.parse(data);
-    const itemById = existingData.filter(item => item.id === req.params.id);
-    // console.log(itemById)
-    res.json({
-      data: itemById[0]
-    })
-  });
+  
+  try{
+    const temp = await redis.get(req.params.id);
+    let value = JSON.parse(temp);
+    value.id = req.params.id;
+    res.json({data : value});
+  }
+  catch{
+    res.json({msg : "Internal Server Error"});
+  }
 })
 
 app.get('/getAllDataByCategory/:cat', async (req, res) => {
-  const filePath = 'data.json';
+  try{
+    console.log(req.params.cat);
+    const keys = await redis.keys('*');
+    console.log(keys);
 
-  // Read the existing JSON file
-  fs.readFile(filePath, 'utf8', (readErr, data) => {
-    if (readErr) {
-      console.error('Error reading the file:', readErr);
-      return;
+    const keys2 = await redisimg.keys('*');
+    console.log(keys2);
+    // Fetch values for each key
+    let data = []
+    for(key of keys)
+    {
+      console.log(key);
+      console.log(key.substring(0,3));
+      if(key.substring(0,3)=="img")
+      continue;
+
+      const temp = await redis.get(key);
+      const value = JSON.parse(temp);
+      console.log(value.category);
+      console.log(req.params.cat);
+      if(value.category === req.params.cat)
+      {
+        value.id = key;
+        data.push(value);
+      }
     }
 
-    const existingData = JSON.parse(data);
-    const itemById = existingData.filter(item => item.category === req.params.cat);
-    // console.log(itemById)
-    res.json({
-      data: itemById
-    })
-  });
+    // Log or use the data as needed
+    console.log('All data:', data);
+    res.json({data : data});
+  }
+  catch(err)
+  {
+    res.json({msg : "internal server error"});
+  }
 })
 
 app.get('/getAllData', async (req, res) => {
-  const filePath = 'data.json';
+  try{
+    const keys = await redis.keys('*');
+    console.log(keys);
 
-  // Read the existing JSON file
-  fs.readFile(filePath, 'utf8', (readErr, data) => {
-    if (readErr) {
-      console.error('Error reading the file:', readErr);
-      return;
+    const keys2 = await redisimg.keys('*');
+    console.log(keys2);
+    // Fetch values for each key
+    let data = []
+    for(key of keys)
+    {
+      console.log(key);
+      console.log(key.substring(0,3));
+      if(key.substring(0,3)=="img")
+      continue;
+
+      const temp = await redis.get(key);
+      const value = JSON.parse(temp);
+      value.id = key;
+      data.push(value);
     }
 
-    const existingData = JSON.parse(data);
+    // Log or use the data as needed
+    console.log('All data:', data);
+    res.json({data : data});
+  }
+  catch(err){
+    res.json({msg : "Internal Server Error"});
+  }
+})
+app.delete('/flushAllData', async (req, res) => {
+  try{
+    // Flush all keys and values from all databases
+  await redis.flushall();
+  res.json({msg : "data deleted successfully"});
 
-    res.json({
-      data: existingData
-    })
-  });
+  }
+  catch(err){
+    res.json({msg : "Internal Server Error"});
+  }
 })
 
-app.get('/getImage/:filename', (req, res) => {
-  const imageFile = fs.readFileSync(__dirname + '/images/' + req.params.filename);
+app.get('/getImage/:imgname', async(req, res) => {
+  const temp = req.params.imgname;
+  const imageFile = await redisimg.getBuffer(temp);
   const base64Data = Buffer.from(imageFile).toString('base64');
   res.json({ imageData: `data:image/jpeg;base64,${base64Data}` });
 });
 
-app.get('/getDesc/:filename', (req, res) => {
-  const fetchedDescription = fs.readFileSync(__dirname + '/desc/' + req.params.filename, 'utf-8');
-  res.json({ fileData: fetchedDescription });
-});
+// app.get('/getDesc/:filename', (req, res) => {
+//   const fetchedDescription = fs.readFileSync(__dirname + '/desc/' + req.params.filename, 'utf-8');
+//   res.json({ fileData: fetchedDescription });
+// });
 
 
 // Start the server
